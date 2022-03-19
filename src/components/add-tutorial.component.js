c54df8cd5216e113887c0ef29986b435a6335822
ohhaithere <frostmasterwork@gmail.com>
2022-03-19 19:20:45 +0300

    Make work with user service

---
+++ src/components/add-tutorial.component.js
@@ -0,0 +1,155 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+
+export default class AddUser extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.saveTutorial = this.saveTutorial.bind(this);
+    this.newTutorial = this.newTutorial.bind(this);
+
+    this.state = {
+      id: "",
+      name: "",
+      role: "",
+      email: "",
+      password: "",
+      submitted: false
+    };
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      role: e.target.value
+    });
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  onChangePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  saveTutorial() {
+    var data = {
+      name: this.state.name,
+      role: this.state.role,
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    TutorialDataService.create(data)
+      .then(response => {
+        this.setState({
+          id: response.data.id,
+          name: response.data.name,
+          role: response.data.role,
+          password: response.data.password,
+          email: response.data.email,
+          submitted: true
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  newTutorial() {
+    this.setState({
+      id: null,
+      name: "",
+      role: "",
+      password: "",
+      email: ""
+    });
+  }
+
+  render() {
+    return (
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            <h4>You submitted successfully!</h4>
+            <button className="btn btn-success" onClick={this.newTutorial}>
+              Add
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                required
+                value={this.state.name}
+                onChange={this.onChangeTitle}
+                name="title"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                required
+                value={this.state.role}
+                onChange={this.onChangeDescription}
+                name="description"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  required
+                  value={this.state.email}
+                  onChange={this.onChangeEmail}
+                  name="email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                  type="text"
+                  className="form-control"
+                  id="password"
+                  required
+                  value={this.state.password}
+                  onChange={this.onChangePassword}
+                  name="password"
+              />
+            </div>
+
+            <button onClick={this.saveTutorial} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
